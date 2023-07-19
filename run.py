0e845974f265cdd8a19604e96456ec75aa50d47a
JKirkby95 <>
2023-07-19 18:19:42 +0000

    get stock from stock sheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -57,7 +58,24 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("updated successfully")
 
+def calculate_surplus_data(sales_row):
+    """
+    Calculating surplus using subtraction
+    """
+    print("Calculating surplus data...")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
